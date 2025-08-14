e68bf881db7db7e5249de4d311219f53c07e2789
DESKTOP-G7K0KAQ\Test <dh1295@york.ac.uk>
2025-08-14 23:05:59 +0100

    added success page added message for when dupe emails are used

--- sql-connect/src/main/java/com/example/sql_connect/UserController.java
+++ sql-connect/src/main/java/com/example/sql_connect/UserController.java
@@ -1,8 +1,10 @@
 package com.example.sql_connect;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,6 +32,15 @@ public class UserController {
 
         } catch (DataIntegrityViolationException e) {
             System.err.println("Database constraint violation: " + e.getMessage());
+            Throwable cause = e.getCause();
+            if (cause instanceof ConstraintViolationException){
+                String constraintName = ((ConstraintViolationException) cause).getConstraintName();
+                if ("customers.email_address_UNIQUE".equals(constraintName)){
+                    return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("This email is already registered");
+                }
+            }
             return ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
                     .body("Some fields are incorrect");
